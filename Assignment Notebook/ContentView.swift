11885a29a9ecac5834b13b08bd7a17032ece443e
Daksh <dnakra@bsd220.org>
2023-01-27 18:46:04 -0600

    Finished steps 1-5, forgot to commit the other 4- whoops

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDoItems =
+
+            [ToDoItem(course: "High", description: "Take out trash", dueDate: Date()),
+
+             ToDoItem(course: "Medium", description: "Pick up clothes", dueDate: Date()),
+
+             ToDoItem(course: "Low", description: "Eat a donut", dueDate: Date())]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(toDoItems) { item in
+                    Text(item.description)
+                }
+                .onMove { indices, newOffset in
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    toDoItems.remove(atOffsets: indexSet)
+                }
+            }
+            .navigationBarTitle("Things", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
@@ -23,4 +39,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
